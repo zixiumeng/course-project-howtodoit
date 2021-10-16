9888f7199d489033b3a56fc78e99f81b39fdfbd6
Richard Soma <85967154+richard-eipi@users.noreply.github.com>
2021-10-16 09:51:21 -0400

    Update Retime.java

--- src/main/java/commands/Retime.java
+++ src/main/java/commands/Retime.java
@@ -1,4 +1,33 @@
 package commands;
 
-public class Retime {
+import driver.DataAccessor;
+import todoSystem.Task;
+import todoSystem.TodoSystem;
+
+/**
+ * This class updates the due date of a task.
+ */
+public class Retime implements Executable {
+
+    /**
+     * This function executes the retime command: change the due date of a task
+     * The task must already exist in the system.
+     *
+     * @param args a list of Strings with length 2, representing user arguments
+     * @return a String indicating a task's due date has been updated
+     */
+    @Override
+    public String execute(DataAccessor dataAccessor, String[] args) {
+        TodoSystem todoSystem = dataAccessor.getSystem(); // Get access to entities
+        // checkArgs(todoSystem, args); // Check whether arguments are valid
+
+        // Map user arguments to task name and new due date
+        String name = args[0];
+        String newtime = args[1];
+        // Get task and change its due date
+        Task task = todoSystem.getTasks().get(name);
+        task.setDueDate(newtime);
+
+        return "The due date of task <" + name + "> has been updated successfully.";
+    }
 }
